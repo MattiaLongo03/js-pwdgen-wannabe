2ececafae7c2ad54e26e94faffbebf509f619706
MattiaLongo03 <mattilongo2003@gmail.com>
2022-10-19 14:53:43 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito22
+*/
+
+let nome = prompt('Qual è il tuo nome?');
+let cognome = prompt('Qual è il tuo cognome?');
+let colore = prompt('Qual è il tuo colore preferuti?');
+let password = nome+cognome+colore+22;
+document.getElementById('password').innerHTML = password;
+console.log('password');
